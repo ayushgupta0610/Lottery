94272c953110223543e001a5938a26704d156c23
ayush <ayushgupta0610@gmail.com>
2018-03-18 03:13:54 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Lottery
+Display of simple lottery contract using solidity (Metamask plugin required) and react
+
+Simple solidity contract is deployed on the address 0x1e0eee0b77C145B9c70d76012E782A51ADb7A9F2 on the rinkeby test network.
+
+The lottery contract allows:
+1. Players to be added to the lottery game (A player needs a minimum of .01 ether to add to the pool to enter the lottery game)
+2. The manager (i.e. the contract owner) to pick the winner for the current lottery which will then reset the game to start afresh
---
+++ src/lottery.js
@@ -0,0 +1,7 @@
+import web3 from './web3';
+
+const address = '0x1e0eee0b77C145B9c70d76012E782A51ADb7A9F2';
+
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":true,"stateMutability":"payable","type":"constructor"}]
+
+export default new web3.eth.Contract(abi, address);
