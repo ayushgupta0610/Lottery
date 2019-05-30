ad64b2ab3df9f9e7e884ab60c38f880a1cf79c71
Ayush Gupta <ayush@aicumen.com>
2019-05-30 16:39:25 +0530

    Adding an end point to get lucky

--- server.js
+++ server.js
@@ -19,5 +19,14 @@ app.get('/revoke/:uin/:agencyID', function(req, res) {
 //	res.send(200);
 });
 
+app.get('/announceWinner/:program/:roundNo', function(req, res) {
+	let result = {
+		'programNameAndRound': req.params.program + req.params.roundNo
+		// 'roundNo': req.params.roundNo
+	}
+	let resJson = JSON.stringify(result);
+	res.send(resJson);
+});
+
 app.listen(process.env.PORT || 3001);
 console.log(`Server is now listening...`);
